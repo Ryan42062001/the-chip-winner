a0e60215e42ca05eb8d1884598b1a3513a320196
Ryan <ktmzgqkzqv@privaterelay.appleid.com>
2026-09-04 23:23:17 -0400

    Clear weekly projection update receipts with local data

--- src/application/local-data-manager.js
+++ src/application/local-data-manager.js
@@ -1,4 +1,15 @@
+const DEFAULT_LOCAL_DATA_KEYS = Object.freeze([
+  "chip-winner:weekly-projection-updates:v1"
+]);
+
 export class LocalDataManager {
-  constructor({ providers = [], storage = globalThis.localStorage, extraKeys = [] } = {}) { this.providers = providers; this.storage = storage; this.extraKeys = extraKeys; }
-  clearAll() { for (const provider of this.providers) provider?.clearCache?.(); for (const key of this.extraKeys) this.storage?.removeItem(key); }
+  constructor({ providers = [], storage = globalThis.localStorage, extraKeys = [] } = {}) {
+    this.providers = providers;
+    this.storage = storage;
+    this.extraKeys = [...new Set([...DEFAULT_LOCAL_DATA_KEYS, ...extraKeys])];
+  }
+  clearAll() {
+    for (const provider of this.providers) provider?.clearCache?.();
+    for (const key of this.extraKeys) this.storage?.removeItem(key);
+  }
 }
